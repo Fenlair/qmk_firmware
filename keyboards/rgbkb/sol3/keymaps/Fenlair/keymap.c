c84237d0e49ddba8eb21f4cab063c95465154f41
Fenlair <fenlair@outlook.com>
2022-03-15 13:04:16 +0100

    Update keymap.c

--- keyboards/rgbkb/sol3/keymaps/Fenlair/keymap.c
+++ keyboards/rgbkb/sol3/keymaps/Fenlair/keymap.c
@@ -25,6 +25,11 @@ enum sol_layers {
 #define ALT_L  LALT_T(KC_L)
 #define GUI_SC LGUI_T(KC_SCLN)
 
+// Layer keys
+#define KC_GAME TG(_GAME)
+#define KC_ADJ  TG(_ADJUST)
+//#define KC_NAV LT(NAVR, KC_SPC)
+
 
 /* Qwerty
  * ┌──────┬──────┬──────┬──────┬──────┬──────┬──────┐  ┌──────┬──────┬──────┬──────┬──────┬──────┬──────┐
@@ -36,7 +41,7 @@ enum sol_layers {
  * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤  ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤
  * │Shift │   Z  │   X  │   C  │   V  │   B  │   {  │  │   }  │   N  │   M  │   ,  │   .  │   /  │Enter │
  * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤  ├──────┼──────┼──────┼──────┼──────┼──────┼──────┤
- * │      │  Win │  Alt │  RGB │ ADJ  │      │ PgDn │  │ PgUp |      │ TG(1)│ TG(2)│ Down │ Up   │      │
+ * │      │  Win │  Alt │  RGB │ ADJ  │      │ PgDn │  │ PgUp |      │ TG(1)│ TG(2)│ Down │ TG(3)│      │
  * └──────┴──────┴──────┴──────┴──────┤ Space├──────┤  ├──────┤ BkSp ├──────┴──────┴──────┴──────┴──────┘
  *                                    │      │ DEL  │  │ Enter│      │
  *                                    └──────┴──────┘  └──────┴──────┘
@@ -60,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                  KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         FN_CAPS,  GUI_A,   ALT_S,   CTL_D,   SFT_F,   KC_G,    KC_LPRN,                  KC_RPRN, KC_H,    SFT_J,   CTL_K,   ALT_L,   GUI_SC,  KC_QUOT,
         KC_LSFT,  KC_Z,    ALT_X,   KC_C,    KC_V,    KC_B,    KC_LCBR,                  KC_RCBR, KC_N,    KC_M,    KC_COMM, ALT_DT,  KC_SLSH, KC_SFTENT,
-        KC_LCTL,  KC_LGUI, KC_LALT, RGB_TOG, ADJUST,  KC_SPC,  KC_PGDN, KC_DEL, KC_ENT,  KC_PGUP, KC_SPC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_RCTL,
+        KC_LCTL,  KC_LGUI, KC_LALT, RGB_TOG, ADJUST,  KC_SPC,  KC_PGDN, KC_DEL, KC_ENT,  KC_PGUP, KC_SPC,  KC_GAME, KC_DOWN, KC_UP,   KC_ADJ,  KC_RCTL,
 
         KC_VOLD, KC_VOLU, KC_VOLD, KC_VOLU, KC_VOLD, KC_VOLU,                                     KC_VOLD, KC_VOLU, KC_VOLD, KC_VOLU, KC_VOLD, KC_VOLU,
         KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY, KC_MPRV,                                                       KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY, KC_MPRV
@@ -72,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, _______,
         _______, KC_A,    KC_S,    KC_D,    KC_F,    _______, _______,                   _______, _______, KC_J,    KC_K,    KC_L,    KC_SCLN, _______,
         _______, _______, KC_X,    _______, _______, _______, _______,                   _______, _______, _______, _______, KC_DOT,  _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, KC_SPC,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 
         _______, _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,                                                       _______, _______, _______, _______, _______
@@ -93,8 +98,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,                    KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
         _______, RGB_SAD, RGB_VAI, RGB_SAI, RESET,   _______, _______,                   _______, _______, KC_P7,   KC_P8,   KC_P9,   _______, _______,
         _______, RGB_HUD, RGB_VAD, RGB_HUI, RGB_RST, _______, DM_REC1,                   _______, _______, KC_P4,   KC_P5,   KC_P6,   _______, _______,
-        _______, RGB_SPD, _______, RGB_SPI, _______, _______, DM_RSTP,                   _______, _______, KC_P1,   KC_P2,   KC_P3,   _______, GAME,
-        _______, RGB_RMOD,RGB_TOG, RGB_MOD, _______, _______, _______, _______, _______, _______, _______, KC_P0,   KC_PDOT, KC_NLCK, QWERTY,  COLEMAK,
+        _______, RGB_SPD, _______, RGB_SPI, _______, _______, DM_RSTP,                   _______, _______, KC_P1,   KC_P2,   KC_P3,   _______, _______,
+        _______, RGB_RMOD,RGB_TOG, RGB_MOD, _______, _______, _______, _______, _______, _______, _______, KC_P0,   KC_PDOT, KC_NLCK, KC_ADJ,  _______,
 
         _______, _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,                                                       _______, _______, _______, _______, _______
