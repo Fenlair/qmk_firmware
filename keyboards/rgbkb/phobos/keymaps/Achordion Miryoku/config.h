67fe36d7c179106510859647b92a609ec43bdd33
Fenlair <fenlair@outlook.com>
2023-05-22 14:52:16 +0200

    Update config.h
    Added ONESHOT_TAP_TOGGLE.

--- keyboards/rgbkb/phobos/keymaps/Achordion Miryoku/config.h
+++ keyboards/rgbkb/phobos/keymaps/Achordion Miryoku/config.h
@@ -15,8 +15,11 @@
 #define NO_ACTION_FUNCTION
 
 // Tap-hold configuration for home row mods.
-#define TAPPING_TERM 180
+#define TAPPING_TERM 180            // Time (in ms) until a tap is interpreted as a hold (default 200)
 #define TAPPING_TERM_PER_KEY
 #define PERMISSIVE_HOLD
-#define QUICK_TAP_TERM 100  // auto-repeat
+#define QUICK_TAP_TERM 100          // auto-repeat
 #define QUICK_TAP_TERM_PER_KEY
+
+#define ONESHOT_TAP_TOGGLE 3        // Tapping this number of times holds the key until tapped once again.
+//#define ONESHOT_TIMEOUT 5000        // Time (in ms) before the one shot key is released
