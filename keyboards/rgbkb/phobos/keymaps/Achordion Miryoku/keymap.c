dab35c63f1926c438a7096d3c685d4d6e4f4c526
Fenlair <fenlair@outlook.com>
2023-06-01 17:56:50 +0200

    Update keymap.c

--- keyboards/rgbkb/phobos/keymaps/Achordion Miryoku/keymap.c
+++ keyboards/rgbkb/phobos/keymaps/Achordion Miryoku/keymap.c
@@ -113,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,                    KC_F11,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
         _______, KC_HOME, KC_UP,   KC_END,  _______, _______, KC_F12,                    KC_F12,  KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_PSCR, _______,
         _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_F13,                    KC_F13,  KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_INS,  _______,
-        _______, AU_TOG,  MU_TOG,  MU_MOD,  _______, _______, KC_F14,                    KC_F14,  _______, _______, _______, _______, _______, _______,
+        _______, AU_TOG,  MU_TOG,  MU_MOD,  _______, _______, KC_F14,                    KC_F14,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
                                    _______, _______, _______, _______, _______, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU,
 
         _______, _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______,
@@ -121,21 +121,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     
     [_SYM] = LAYOUT(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,                    KC_F11,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
-        _______, _______, KC_LABK, KC_RABK, KC_AT,   _______, KC_F12,                    KC_F12,  _______, KC_P7,   KC_LBRC, KC_RBRC, _______, _______,
-        KC_CAPS, KC_EXLM, KC_MINS, KC_PLUS, KC_EQL , KC_HASH, KC_F13,                    KC_F13,  _______, KC_P4,   KC_LPRN, KC_RPRN, _______, _______,
-        _______, _______, KC_SLSH, KC_ASTR, KC_CIRC, _______, KC_F14,                    KC_F14,  _______, KC_P1,   KC_LCBR, KC_RCBR, _______, _______,
-                                   KC_PGDN, KC_PGUP, _______, _______, _______, _______, _______, _______, KC_HOME, KC_END,
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,                    KC_F11,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  XXXXXXX,
+        _______, _______, KC_LABK, KC_RABK, KC_AT,   _______, KC_F12,                    KC_F12,  XXXXXXX, KC_P7,   KC_LBRC, KC_RBRC, XXXXXXX, KC_DEL,
+        KC_CAPS, KC_EXLM, KC_MINS, KC_PLUS, KC_EQL , KC_HASH, KC_F13,                    KC_F13,  XXXXXXX, KC_P4,   KC_LPRN, KC_RPRN, XXXXXXX, XXXXXXX,
+        _______, _______, KC_SLSH, KC_ASTR, KC_CIRC, _______, KC_F14,                    KC_F14,  XXXXXXX, KC_P1,   KC_LCBR, KC_RCBR, XXXXXXX, _______,
+                                   KC_PGDN, KC_PGUP, KC_UNDS, _______, _______, _______, _______, _______, KC_HOME, KC_END,
 
         _______, _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,                                                       _______, _______, _______, _______, _______
     ),
     
     [_NUM] = LAYOUT(
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,                    KC_F12,  KC_F6,   KC_F7,   KC_F8,  KC_F9,   KC_F10,  _______,
-        _______, _______, _______, _______, _______, _______, _______,                   _______, _______, KC_P7,   KC_P8,  KC_P9,   _______, _______,
-        _______, _______, _______, _______, _______, _______, _______,                   _______, _______, KC_P4,   KC_P5,  KC_P6,   _______, _______,
-        _______, _______, _______, _______, _______, _______, _______,                   _______, _______, KC_P1,   KC_P2,  KC_P3,   _______, _______,
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,                    KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
+        _______, _______, _______, _______, _______, _______, _______,                   _______, _______, KC_P7,   KC_P8,   KC_P9,   _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,                   _______, _______, KC_P4,   KC_P5,   KC_P6,   _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,                   _______, _______, KC_P1,   KC_P2,   KC_P3,   _______, _______,
                                    _______, _______, _______, _______, _______, _______, _______, _______, KC_P0,   KC_PDOT,
 
         _______, _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______,
@@ -143,11 +143,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_ADJUST] = LAYOUT(
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,                    KC_F12,  KC_F6,   KC_F7,   KC_F8,  KC_F9,   KC_F10,  _______,
-        _______, RGB_SAD, RGB_VAI, RGB_SAI, QK_BOOT, _______, _______,                   _______, _______, KC_P7,   KC_P8,  KC_P9,   _______, _______,
-        _______, RGB_HUD, RGB_VAD, RGB_HUI, RGB_RST, _______, DM_REC1,                   _______, _______, KC_P4,   KC_P5,  KC_P6,   _______, _______,
-        _______, RGB_SPD, _______, RGB_SPI, _______, _______, DM_RSTP,                   _______, _______, KC_P1,   KC_P2,  KC_P3,   _______, _______,
-                                   _______, _______, _______, _______, _______, _______, _______, _______, KC_P0,   KC_PDOT,
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,                    KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
+        _______, RGB_SAI, RGB_VAI, RGB_HUI, RGB_SPI, QK_BOOT, _______,                   _______, _______, _______, _______, _______, _______, _______,
+        _______, RGB_SAD, RGB_VAD, RGB_HUD, RGB_SPD, RGB_RST, _______,                   _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, _______,
+                                   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 
         _______, _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,                                                       _______, _______, _______, _______, _______
@@ -295,5 +295,15 @@ void rgb_matrix_indicators_user(void) {
     }
     if(IS_LAYER_ON(_ADJUST)) {
         rgb_matrix_set_color( 48, RGB_MAGENTA);
+        rgb_matrix_set_color( 49, RGB_RED);
+        rgb_matrix_set_color( 60, RGB_ORAGNE);
+        rgb_matrix_set_color( 53, RGB_GREEN);
+        rgb_matrix_set_color( 52, RGB_GREEN);
+        rgb_matrix_set_color( 51, RGB_GREEN);
+        rgb_matrix_set_color( 50, RGB_GREEN);
+        rgb_matrix_set_color( 56, RGB_BLUE);
+        rgb_matrix_set_color( 57, RGB_BLUE);
+        rgb_matrix_set_color( 58, RGB_BLUE);
+        rgb_matrix_set_color( 59, RGB_BLUE);
     }
 }
