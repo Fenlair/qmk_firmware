fc9d848bd8a6f22cdd2fc5776080808cf1cd1999
Fenlair <fenlair@outlook.com>
2023-06-02 08:34:32 +0200

    Update keymap.c

--- keyboards/rgbkb/phobos/keymaps/Achordion Miryoku/keymap.c
+++ keyboards/rgbkb/phobos/keymaps/Achordion Miryoku/keymap.c
@@ -111,9 +111,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_NAV] = LAYOUT(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,                    KC_F11,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
-        _______, KC_HOME, KC_UP,   KC_END,  _______, _______, KC_F12,                    KC_F12,  KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_PSCR, _______,
-        _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_F13,                    KC_F13,  KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_INS,  _______,
-        _______, AU_TOG,  MU_TOG,  MU_MOD,  _______, _______, KC_F14,                    KC_F14,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+        _______, KC_HOME, KC_UP,   KC_END,  XXXXXXX, XXXXXXX, KC_F12,                    KC_F12,  KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_PSCR, _______,
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, KC_F13,                    KC_F13,  KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_INS,  _______,
+        _______, AU_TOG,  MU_TOG,  MU_MOD,  XXXXXXX, XXXXXXX, KC_F14,                    KC_F14,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
                                    _______, _______, _______, _______, _______, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU,
 
         _______, _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______,
